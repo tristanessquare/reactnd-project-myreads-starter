3b515bc043f14449f83762c7afd7a4f500619f87
Tristan Baumbusch <tristan.baumbusch@essquare.de>
2020-04-01 23:01:07 +0200

    feat: created two components BooksApp and Search. Introduced routing.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import BooksApp from './App'
+import Search from './Search'
 import './index.css'
+import { BrowserRouter, Route } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const routing = (
+    <BrowserRouter>
+        <div className="app">
+            <Route exact path="/" component={BooksApp} />
+            <Route exact path="/search" component={Search} />
+        </div>
+    </BrowserRouter>
+)
+
+ReactDOM.render(routing, document.getElementById('root'))
